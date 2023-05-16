b85769a4ed1145ae75062a17b3e777c4c8a578d7
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-05-15 17:04:29 -0700

    reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, Types } = require("mongoose");
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Types.ObjectId,
+      default: new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = ReactionSchema;
